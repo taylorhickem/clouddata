3f315634cd77a31ce079e6e4a0795a3fa3509d78
taylorhickem <taylor.hickem@gmail.com>
2024-02-16 19:18:32 +0800

    skeleton outline GDriveToS3 integration class

--- clouddata/aws/s3.py
+++ clouddata/aws/s3.py
@@ -1,1 +1,32 @@
 import boto3
+
+
+s3_client = None
+
+
+class S3Client(object):
+    connected = False
+
+    def __init__(self):
+        pass
+
+    def login(self):
+        client_login()
+        self.connected = True
+
+    def client(self):
+        return s3_client
+
+    def __exit__(self):
+        client_logout()
+
+
+def client_login():
+    global s3_client
+    if not s3_client:
+        s3_client = boto3.client('s3')
+
+def client_logout():
+    global s3_client
+    if not s3_client:
+        s3_client = None
---
+++ clouddata/integrations.py
@@ -0,0 +1,20 @@
+from .aws.s3 import S3Client
+from . import gdrive
+
+
+class GDriveToS3(object):
+    s3client = None
+    gdclient = None
+    connected = False
+    
+    def __init__(self):
+        self.s3client = S3Client()
+        self.gdclient = gdrive.GDriveClient()
+
+    def login(self):
+        self.s3client.login()
+        self.gdclient.login()
+        self.connected = True
+
+    def __repr__(self):
+        return f'<{self.__class__} Integration object>'
